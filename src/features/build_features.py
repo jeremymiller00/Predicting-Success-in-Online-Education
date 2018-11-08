b569ce021cb9af0b24f7b65c8b1ca0bb845fe330
Jeremy <jeremymiller00@gmail.com>
2018-11-07 16:21:37 -0800

    file upload

---
+++ src/features/build_features.py
@@ -0,0 +1,7 @@
+'''
+Transformations, feature engineering and extraction
+'''
+
+# convert to string? id, module, presentation
+# make dummmies: result, gender, region, highest_ed, imd_band, age_band, diability
+
