7357ae5be0025b94c45c91e098e78def1b1765e6
Jeremy <jeremymiller00@gmail.com>
2018-11-07 17:06:47 -0800

    building feature transformations

--- project_plan.md
+++ project_plan.md
@@ -21,3 +21,7 @@ Calculating final grades requires making some assumptions about assessment weigh
 B and J presentations should be analyzed separately as they may be different in stucture 
 
 Nevertheless, for some presentations the corresponding previous B/J presentation do not exist and therefore the J presentation must be used to inform the B presentation or vice versa. In the dataset this is the case of CCC, EEE and GGG modules.
+
+### Questions about data set
+
+Are the courses undergraduate or postgraduate?
--- src/features/build_features.py
+++ src/features/build_features.py
@@ -3,5 +3,29 @@ Transformations, feature engineering and extraction
 '''
 
 # convert to string? id, module, presentation
+
 # make dummmies: result, gender, region, highest_ed, imd_band, age_band, diability
 
+# fill in null values in imd_band? use other features to predict?
+
+cols = ['gender', 'region', 'highest_education', 'imd_band', 'age_band', 'disability']
+
+def drop_nulls(dataframe):
+    '''
+    Drops rows with null values from dataframe
+    '''
+    return dataframe.dropna(axis = 0)
+
+
+def one_hot(dataframe, columns):
+    '''
+    Concatenates dummy variable (one-hot encoded) columns onto original dataframe for specified columns. Original columns are dropped.
+
+    Parameters:
+    ----------
+    input {dataframe}: 
+    output {list}: list of columns to be one-hot encoded and dropped
+    '''
+
+    dumms = pd.get_dummies(dataframe[columns], dummy_na=True, drop_first=True)
+    return pd.concat([dataframe, dumms], axis = 1)
