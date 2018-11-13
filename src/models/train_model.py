0e66cda799b98fb1d1349a5049da5babf1e72e8b
Jeremy <jeremymiller00@gmail.com>
2018-11-12 20:10:23 -0800

    update

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -173,7 +173,7 @@ if __name__ == '__main__':
 
     svc_params = {
             'C': [0.001, 0.01, 0.1, 1, 10, 100],
-            'kernel': ['linear']''
+            'kernel': ['linear'],
             'degree': [1, 3, 5],
             'gamma': [0.001, 0.01, 0.1],
             'probability': ['True'],
