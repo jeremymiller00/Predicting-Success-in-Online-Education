651ac061eccd4003f2c06753307136f6eabacd98
Ubuntu <ubuntu@ip-172-31-21-154.ec2.internal>
2018-11-13 20:59:05 +0000

    lr.py

---
+++ lr.py
@@ -0,0 +1,5 @@
+LogisticRegression(C=1, class_weight=None, dual=False, fit_intercept=True,
+          intercept_scaling=1, max_iter=200, multi_class='warn',
+          n_jobs=None, penalty='l2', random_state=None, solver='newton-cg',
+          tol=0.0001, verbose=0, warm_start='False')
+
