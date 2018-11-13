270f6bf0b72f129a7dad092d4780b6e7ddd04d40
Jeremy <jeremymiller00@gmail.com>
2018-11-13 11:33:15 -0800

    remove date unreg from x_train

--- src/models/train_model_lr.py
+++ src/models/train_model_lr.py
@@ -81,6 +81,7 @@ if __name__ == '__main__':
     # fill and scale
     # X_train_mini.fillna(value = 0, inplace = True)
     # X_train_mini = scale_subset(X_train_mini, numeric_cols)
+    X_train.drop('date_unregistration', axis = 1, inplace = True)
     X_train.fillna(value = 0, inplace = True)
     X_train = scale_subset(X_train, numeric_cols)
 
