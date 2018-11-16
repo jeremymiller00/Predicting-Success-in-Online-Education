b1f97e2f05dd5a65c9a7955457dcead5bc59e405
Jeremy <jeremymiller00@gmail.com>
2018-11-16 12:07:26 -0800

    add evaluation metrics

--- src/models/train_model_lr.py
+++ src/models/train_model_lr.py
@@ -7,7 +7,7 @@ import pandas as pd
 import pickle
 
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import make_scorer, confusion_matrix, recall_score, roc_auc_score, roc_curve, recall_score
+from sklearn.metrics import make_scorer, confusion_matrix, recall_score, roc_auc_score, roc_curve, recall_score, classification_report
 from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import LogisticRegression
 
@@ -49,6 +49,22 @@ def standard_confusion_matrix(y_true, y_pred):
     """
     [[tn, fp], [fn, tp]] = confusion_matrix(y_true, y_pred)
     return np.array([[tp, fp], [fn, tn]])
+
+def print_roc_curve(y_test, probabilities):
+    '''
+    Calculates and prints a ROC curve given a set of test classes and probabilities from a trained classifier
+    '''
+    tprs, fprs, thresh = roc_curve(y_test, probabilities)
+    plt.figure(figsize=(12,10))
+    plt.plot(fprs, tprs, 
+         label='Logistic Regression', 
+         color='red')
+    plt.plot([0,1],[0,1], 'k:')
+    plt.legend()
+    plt.xlabel("FPR")
+    plt.ylabel("TPR")
+    plt.title("ROC Curve AUC: {} Recall: {}".format(roc_auc, recall))
+    plt.show()
 
 ######################################################################
 
@@ -92,6 +108,9 @@ if __name__ == '__main__':
 
     log_reg_model = lr_clf.best_estimator_
 
+    # best model as determined by grid search
+    # model = LogisticRegression(C=1, class_weight=None, dual=False,    fit_intercept=True, intercept_scaling=1, max_iter=200,            multi_class='warn', n_jobs=None, penalty='l2', random_state=None, solver='newton-cg', tol=0.0001, verbose=0, warm_start='False')
+
     # save model
     pickle.dump(log_reg_model, open('models/logistic_regression_completion.p', 'wb'))
 
@@ -101,20 +120,21 @@ if __name__ == '__main__':
     probas = log_reg_model.predict_proba(X_test)[:, 1:]
     tprs, fprs, thresh = roc_curve(y_test, probas)
     recall = recall_score(y_test, predictions)
+    conf_mat = standard_confusion_matrix(y_test, predictions)
+    class_report = classification_report(y_test, predictions)
 
+    print_roc_curve(y_test, probas)
     print('Best Model: {}'.format(log_reg_model))
     print('Best Model parameters: {}'.format(lr_clf.best_params_))
     print('Best Model Log Loss: {}'.format(lr_clf.best_score_))
     print('Roc Auc: {}'.format(roc_auc))
     print('Recall Score: {}'.format(recall))
+    print('Confusion Matrix: {}'.format(conf_mat))
+    print('Classification Report: {}'.format(class_report))
 
-    # plt.figure(figsize=(12,10))
-    # plt.plot(fprs, tprs, 
-    #      label='Logistic Regression', 
-    #      color='red')
-    # plt.plot([0,1],[0,1], 'k:')
-    # plt.legend()
-    # plt.xlabel("FPR")
-    # plt.ylabel("TPR")
-    # plt.title("ROC Curve AUC: {} Recall: {}".format(roc_auc, recall))
-    # plt.show()
+    # Feature Importances
+    abs_coef = list(np.abs(log_reg_model.coef_.ravel()))
+    features = list(X_test.columns)
+    coef_dict = c.OrderedDict((zip(abs_coef, features)))
+    ordered_feature_importances = sorted(coef_dict.items(), reverse=True)
+    print('The top ten features affecting completion are: {}'.format( ordered_feature_importances[:10])
