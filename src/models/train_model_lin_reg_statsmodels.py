54c5c77f3c1715ef4e7aa9dafc56fde44d8bd279
Jeremy <jeremymiller00@gmail.com>
2018-11-19 08:48:12 -0800

    sklearn wrapper for statsmodels ols

--- src/models/train_model_lin_reg_statsmodels.py
+++ src/models/train_model_lin_reg_statsmodels.py
@@ -7,12 +7,26 @@ import pickle
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, explained_variance_score, r2_score
+from sklearn.base import BaseEstimator, RegressorMixin
 # from sklearn.model_selection import GridSearchCV
-# from sklearn.linear_model import LogisticRegression
 import statsmodels.api as sm
 # import scipy.stats as scs
 import matplotlib.pyplot as plt
-%matplotlib inline
+
+class SMWrapper(BaseEstimator, RegressorMixin):
+    """ A universal sklearn-style wrapper for statsmodels regressors """
+    def __init__(self, model_class, fit_intercept=True):
+        self.model_class = model_class
+        self.fit_intercept = fit_intercept
+    def fit(self, X, y):
+        if self.fit_intercept:
+            X = sm.add_constant(X)
+        self.model_ = self.model_class(y, X)
+        self.results_ = self.model_.fit()
+    def predict(self, X):
+        if self.fit_intercept:
+            X = sm.add_constant(X)
+        return self.results_.predict(X)
 
 def scale_subset(df, columns):
     '''
@@ -50,39 +64,39 @@ def only_completed(X_train, y_train, X_test, y_test, y_train_not_comp, y_test_no
 
     return X_train.drop(train_indices), y_train.drop(train_indices), X_test.drop(test_indices), y_test.drop(test_indices)
 
-
+%reset
 ######################################################################
 
 if __name__ == '__main__':
 
-    X_train = pd.read_csv('data/processed/X_train.csv')
-    y_train = pd.read_csv('data/processed/y_train.csv')
+    X_train = pd.read_csv('data/processed/first_half/X_train.csv')
+    y_train = pd.read_csv('data/processed/first_half/y_train.csv')
     y_train_not_comp = y_train[['module_not_completed']]
     y_train = y_train['estimated_final_score']
-    X_test = pd.read_csv('data/processed/X_test.csv')
-    y_test = pd.read_csv('data/processed/y_test.csv')
+    X_test = pd.read_csv('data/processed/first_half/X_test.csv')
+    y_test = pd.read_csv('data/processed/first_half/y_test.csv')
     y_test_not_comp = y_test[['module_not_completed']]
     y_test = y_test['estimated_final_score']
 
-    numeric_cols = ['num_of_prev_attempts', 'studied_credits', 'clicks_per_day', 'pct_days_vle_accessed','max_clicks_one_day','first_date_vle_accessed', 'avg_score', 'avg_days_sub_early','days_early_first_assessment', 'score_first_assessment']
+    numeric_cols = ['num_of_prev_attempts', 'studied_credits', 'module_presentation_length', 'sum_click_dataplus', 'sum_click_dualpane', 'sum_click_externalquiz', 'sum_click_forumng','sum_click_glossary', 'sum_click_homepage', 'sum_click_htmlactivity', 'sum_click_oucollaborate', 'sum_click_oucontent', 'sum_click_ouelluminate', 'sum_click_ouwiki', 'sum_click_page', 'sum_click_questionnaire', 'sum_click_quiz', 'sum_click_repeatactivity', 'sum_click_resource', 'sum_click_sharedsubpage', 'sum_click_subpage', 'sum_click_url', 'sum_days_vle_accessed', 'max_clicks_one_day', 'first_date_vle_accessed', 'avg_score', 'avg_days_sub_early', 'days_early_first_assessment', 'score_first_assessment']
 
     # fill and scale
     X_train.fillna(value = 0, inplace = True)
     y_train.fillna(value = 0, inplace = True)
     X_train = scale_subset(X_train, numeric_cols)
-    X_train = sm.add_constant(X_train)
+    # X_train = sm.add_constant(X_train)
     X_test.fillna(value = 0, inplace = True)
     y_test.fillna(value = 0, inplace = True)
     X_test = scale_subset(X_test, numeric_cols)
-    X_test = sm.add_constant(X_test)
+    # X_test = sm.add_constant(X_test)
 
     # only students who completed the course
     X_train, y_train, X_test, y_test = only_completed(X_train, y_train, X_test, y_test, y_train_not_comp, y_test_not_comp)
 
     # estimator
-    lin_reg = sm.OLS(y_train, X_train)
+    lin_reg = SMWrapper(sm.OLS)
 
-    lin_reg = lin_reg.fit()
+    lin_reg.fit(X_train, y_train)
 
     lin_reg.summary()
 
@@ -90,7 +104,7 @@ if __name__ == '__main__':
 
     rmse = np.sqrt(mean_squared_error(y_test, predictions))
     print(rmse)
-    explained_variance_score(y_test, predictions)
+    evs = explained_variance_score(y_test, predictions)
     r2_score(y_test, predictions)
 
     residuals = y_test - predictions
@@ -103,7 +117,7 @@ if __name__ == '__main__':
 
 
     # # save model
-    # pickle.dump(log_reg_model, open('models/logistic_regression_completion.p', 'wb'))
+    # pickle.dump(log_reg_model, open('models/linear_regression_first_half.p', 'wb'))
 
     # # evaluation
     # predictions = log_reg_model.predict(X_test)
